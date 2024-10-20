0fe08ce2037d7f797206b5613e4863bd0eaa7cd6
Rahul Narayanan <rahulon@icloud.com>
2024-10-20 12:20:34 -0400

    Initial Commit

---
+++ CognitiveBenchmarkingVision/CognitiveBenchmarkingVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CognitiveBenchmarkingVisionApp.swift
+//  CognitiveBenchmarkingVision
+//
+//  Created by Rahul on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CognitiveBenchmarkingVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CognitiveBenchmarkingVision/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CognitiveBenchmarkingVision
+//
+//  Created by Rahul on 10/20/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
